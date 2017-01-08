a6ab3f572aaa8e3b4fe57a50b6aa64eb7a262997
Craig Sturgis <craig@csturg.is>
2017-01-08 15:43:43 -0500

    hey a working pull showing the standings in order

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var marko = require('marko');
 var FantasySports = require('FantasySports');
+var _ = require('lodash');
 
 /* GET home page. */
 
@@ -10,18 +11,38 @@ router.get('/', function(req, res) {
   //console.log("isAuthenticated: " + FantasySports.isAuthenticated);
 
   try {
-    console.log("trying");
     FantasySports
     .request(req, res)
-    .api('http://fantasysports.yahooapis.com/fantasy/v2/league/nfl.l.318700/?format=json')
+    .api('http://fantasysports.yahooapis.com/fantasy/v2/league/nfl.l.318700/standings;year=2016?format=json')
     .done(function(data) {
-      console.dir(data.fantasy_content.league[0]);
+
+      var leagueObj = data.fantasy_content.league[0];
+      var standingsObj = data.fantasy_content.league[1].standings[0];
+
+      var teamArray = [];
+      //console.dir(leagueObj);
+      _.forEach(standingsObj.teams, function(team) {
+        //console.log(team.team);
+        if (team.team && team.team[0]) {
+          var teamResult = {};
+          console.log(team.team[0]);
+          //console.log(_.find(team.team[0], _.has('name')));
+          _.assign(teamResult, _.find(team.team[0], function(o) {
+            if (o.name) return o;
+          }));
+
+          console.log(teamResult);
+          teamArray.push(teamResult);
+        }
+      });
+
       indexTemplate.render({
         $global: {
           ENV_DEVELOPMENT: req.app.locals.ENV_DEVELOPMENT,
           isAuthenticated: FantasySports.isAuthenticated
         },
-        title: 'Fumbled the Frikkin\' Football'
+        title: leagueObj.name,
+        teams: teamArray
       }, res);
     });
   }
