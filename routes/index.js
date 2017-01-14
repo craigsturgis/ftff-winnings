ba96bfe233d7b7ec18e44c27beacb90aa1deec9f
Craig Sturgis <craig@csturg.is>
2017-01-14 16:23:28 -0500

    working all weekly points winners

--- routes/index.js
+++ routes/index.js
@@ -112,8 +112,8 @@ router.get('/', function(req, res) {
       .request(req, res)
       //.api('http://fantasysports.yahooapis.com/fantasy/v2/team/nfl.l.318700.t.1/stats;year=2016;type=week;week=13?format=json')
       //.api('http://fantasysports.yahooapis.com/fantasy/v2/team/nfl.l.318700.t.1/standings;year=2016;week=3?format=json')
-      //.api('http://fantasysports.yahooapis.com/fantasy/v2/league/nfl.l.318700/scoreboard;year=2016;week=1;week=2;week=3;week=4;week=5;week=6;week=7;week=8;week=9;week=10;week=11;week=12;week=13?format=json')
-      .api('http://fantasysports.yahooapis.com/fantasy/v2/league/nfl.l.318700/scoreboard;year=2016;week=1?format=json')
+      .api('http://fantasysports.yahooapis.com/fantasy/v2/league/nfl.l.318700/scoreboard;year=2016;week=1;week=2;week=3;week=4;week=5;week=6;week=7;week=8;week=9;week=10;week=11;week=12;week=13?format=json')
+      //.api('http://fantasysports.yahooapis.com/fantasy/v2/league/nfl.l.318700/scoreboard;year=2016;week=1;week=2;week=3;week=4?format=json')
       .done(function(muData) {
 
         //console.dir(muData.fantasy_content.league[1].standings[0].teams[7].team[2]);
@@ -138,33 +138,45 @@ router.get('/', function(req, res) {
             var winner = {
               key: matchup.winner_team_key,
               pointsFor: 0,
-              pointsAgainst: 0
+              pointsAgainst: 0,
+              weekNumber: weekNumber
             };
 
-            // console.dir(matchup);
-            // if (matchup && matchup.teams && matchup.teams[0].team[0][0].team_key === winner.key) {
-            //   console.log('!');
-            //   //winner.pointsFor = _.toNumber(matchup.teams[0].team[0].team[1].team_points.total);
-            //   //winner.pointsAgainst = _.toNumber(matchup.teams[0].team[1].team[1].team_points.total);
-            // }
+            //console.dir(matchup);
+            //console.dir(matchup[0].teams[0].team[1].team_points.total);
+
+            if (matchup[0].teams[0].team[0][0].team_key === winner.key) {
+              //console.log('team0');
+              winner.pointsFor = _.toNumber(matchup[0].teams[0].team[1].team_points.total);
+              winner.pointsAgainst = _.toNumber(matchup[0].teams[1].team[1].team_points.total);
+            } else {
+              //console.log('team1');
+              winner.pointsFor = _.toNumber(matchup[0].teams[1].team[1].team_points.total);
+              winner.pointsAgainst = _.toNumber(matchup[0].teams[0].team[1].team_points.total);
+            }
+
+            //console.dir(winner);
+
+
           }
 
+          if (!weekWinners[weekNumber] ||
+            (weekWinners[weekNumber].pointsFor === winner.pointsFor && weekWinners[weekNumber].pointsAgainst < winner.pointsAgainst) ||
+            weekWinners[weekNumber].pointsFor < winner.pointsFor) {
 
+            weekWinners[weekNumber] = winner;
+          }
+        });
 
+        _.forEach(weekWinners, function(weekWinner) {
+          if (weekWinner) {
+            teamMap[weekWinner.key].finalPrizes.push({
+              prizeName: "Week " + weekWinner.weekNumber + " Highest Points: " + weekWinner.pointsFor,
+              winnings: 20
+            });
+            teamMap[weekWinner.key].totalWinnings += 20;
+          }
 
-
-          //console.dir(winner);
-
-
-          // // var matchupWinner = {
-          // //   team_key: matchupData.winner_team_key,
-          // // }
-
-          // if (!weekWinners[weekNumber]) {
-          //   weekWinners[weekNumber] = {
-
-          //   }
-          // }
         });
 
 
