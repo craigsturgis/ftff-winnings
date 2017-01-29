2ed1f6c3f6cacb560bb05da9388689a749b3bfb5
Craig Sturgis <craig@csturg.is>
2017-01-28 23:29:19 -0500

    working rollup of 13 week point totals for blocks of players

--- routes/players.js
+++ routes/players.js
@@ -5,6 +5,7 @@ var _ = require('lodash');
 const marko = require('marko');
 const util = require('util');
 const bbPromise = require("bluebird");
+const cumulativePoints = require("../helpers/cumulativePoints");
 
 
 var playersTemplate = marko.load(require.resolve('../views/players.marko'));
@@ -26,7 +27,8 @@ var apiUrl = [
   ';year=2016?format=json',
 ].join('');
 
-console.log(apiUrl);
+var playerMap = {};
+var playerArray = [];
 
 router.get('/', function(req, res) {
   //console.log("isAuthenticated: " + FantasySports.isAuthenticated);
@@ -40,30 +42,50 @@ router.get('/', function(req, res) {
     var leagueObj = data.fantasy_content.league[0];
     var playersObj = data.fantasy_content.league[1].players;
 
-    var players = [];
-
     _.forEach(playersObj, function(player) {
       if (player.player) {
 
         // console.log(util.inspect(player.player, {depth: 5}));
 
-        players.push({
+        var playerObj = {
           player_key: player.player[0][0].player_key,
           name: player.player[0][2].name.full,
           // position: player.player[0][11].display_position,
           week_17_points: player.player[1].player_points.total,
           owner_team_key: player.player[2].ownership.owner_team_key || null,
           owner_team_name: player.player[2].ownership.owner_team_name || 'NOBODY!',
-          // owner_count: player.player[2].ownership[0],
-        });
+        };
 
-        // console.log('_______________');
+        if (player.player[2].ownership[0]) {
+          // console.log(util.inspect(player.player[2].ownership, {depth: 5}));
+          playerObj.owner_count = player.player[2].ownership[0].teams[0].count;
+        }
+
+        playerArray.push(playerObj);
+        playerMap[playerObj.player_key] = playerObj;
       }
-
     });
 
     //console.log(util.inspect(playersObj, {depth: null}))
-    console.log(util.inspect(players, {depth: null}))
+
+    var keys = _.map(playerArray, function(playerObj) {
+      return playerObj.player_key;
+    });
+
+    return cumulativePoints.playerCumulativePoints(keys, 13, req, res);
+
+  }).then(function(pointsMap) {
+
+    // console.log(util.inspect(pointsMap, {depth: null}))
+
+    var highestPoints = null;
+
+    _.forEach(pointsMap, function(playerTotal) {
+      var player = playerMap[playerTotal.player_key];
+
+      player.week_13_points = playerTotal.pointTotal;
+      player.weeks = playerTotal.weeks;
+    })
 
     playersTemplate.render({
       $global: {
